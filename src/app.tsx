4b064067bc16208e00c18a100e1c0161543e3428
John Ballesteros <johnshift.dev@gmail.com>
2021-10-16 10:20:47 +0800

    initial preact + windicss setup

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+export function App() {
+  return (
+    <>
+      <div 
+        id='using-class'
+        class='flex container mx-auto h-screen justify-center items-center'
+      >
+          <h1>Home Page</h1>
+      </div>
+    </>
+  )
+}
